5ecfefb87c3d4ee6b375b8f84dcd9eaf993fa28f
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-27 14:13:57 +1000

    Removed tests from being compiled by TSC

--- src/js/tests/helpers/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const TEST_FILE = "./src/ts/tests/package/package.json";
-export declare const TEST_BAD_FILE = "./src/ts/tests/package/package.bad.json";
-export declare const TEST_TXT_FILE = "./src/ts/tests/package/test.txt";
-export declare const TEST_TEMP_FILE = "./testPackage.json";
-export declare const TEST_FILE_WITH_DEP = "./src/ts/tests/package/package.with-dep.json";
--- src/js/tests/helpers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEST_FILE = "./src/ts/tests/package/package.json";
-exports.TEST_BAD_FILE = "./src/ts/tests/package/package.bad.json";
-exports.TEST_TXT_FILE = "./src/ts/tests/package/test.txt";
-exports.TEST_TEMP_FILE = "./testPackage.json";
-exports.TEST_FILE_WITH_DEP = "./src/ts/tests/package/package.with-dep.json";
